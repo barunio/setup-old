a372c15e47f84b7205a8072855e2e4e72759e9c5
Barun Singh <bsingh@wegowise.com>
2015-05-28 19:36:07 -0400

    add Postgres

--- scripts/set_startup_services.sh
+++ scripts/set_startup_services.sh
@@ -5,3 +5,6 @@ ln -sfv /usr/local/opt/memcached/*.plist ~/Library/LaunchAgents
 
 echo "Start redis at login"
 ln -sfv /usr/local/opt/redis/*.plist ~/Library/LaunchAgents
+
+echo "Start postgres at login"
+ln -sfv /usr/local/opt/postgresql/*.plist ~/Library/LaunchAgents
