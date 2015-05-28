57d1d7da498a7598788039446bcb88cd607c77de
Barun Singh <bsingh@wegowise.com>
2015-05-28 19:27:55 -0400

    add redis

--- scripts/set_startup_services.sh
+++ scripts/set_startup_services.sh
@@ -2,3 +2,6 @@
 
 echo "Start memcached at login"
 ln -sfv /usr/local/opt/memcached/*.plist ~/Library/LaunchAgents
+
+echo "Start redis at login"
+ln -sfv /usr/local/opt/redis/*.plist ~/Library/LaunchAgents
