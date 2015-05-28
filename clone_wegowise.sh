cee731668f57565c516f6bd73a841f281e389e6f
Barun Singh <bsingh@wegowise.com>
2015-05-28 12:01:25 -0400

    clone Wegowise and Wegotools repos

---
+++ clone_wegowise.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir ~/work
+mkdir ~/work/code
+
+# Clone WegoWise
+mkdir ~/work/code/wegowise
+git clone git@github.com:wegowise/wegowise.git ~/work/code/wegowise
+
+# Clone Wegotools
+mkdir ~/.wego
+git clone git@github.com:wegowise/wegotools.git ~/.wego
