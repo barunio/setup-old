f3b4fcde1b665bd42bcd0af9a9204228e91bd6c2
Barun Singh <bsingh@wegowise.com>
2015-12-26 18:30:34 -0800

    ask user to confirm overriding existing Vim config

--- support/helpers.rb
+++ support/helpers.rb
@@ -39,9 +39,18 @@ def install_homebrew_formulas
 end
 
 def install_vim
+  vimdir = "#{HOME}/.vim"
+  if File.exists? vimdir
+    puts "Overwrite Vim settings? (y/N)"
+    unless %w[y Y].include? gets.chomp
+      puts " >> SKIPPING Vim setup"
+      return
+    end
+  end
+
   put_heading "Trashing old .vim directory and installing vundle in a new one"
   move_to_trash File.join(HOME, '.vim')
-  FileUtils.mkdir "#{HOME}/.vim"
+  FileUtils.mkdir vimdir
   FileUtils.mkdir "#{HOME}/.vim/bundle"
   `git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle`
 end
